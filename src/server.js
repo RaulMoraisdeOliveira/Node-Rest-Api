b392e1cece95bfbd868a25cdd9774f52338de5df
Raul Morais <raulmorais2010@gmail.com>
2026-02-05 20:06:54 -0300

    feat: add logical filter to send users

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const http = require('node:http');
+const users = require('./mocks/users');
 
 http.createServer(function (request, response) {
-  response.write(
+  if (request.url === '/users') {
+    response.writeHead(
+      200, 
+      {'content-type': 'application/json'}
+    );
+    response.end(JSON.stringify(users));
+    return;
+  }
+
+  response.writeHead(
     200, 
-    {'content-type': 'text/html; charset=utf-8'});
-  response.end('<h1>Olá Cliente!</h1>');
+    {'content-type': 'text/html; charset=utf-8'}
+  );
+  response.end('<h1>Olá, Cliente!</h1>');
 }).listen(3000);
